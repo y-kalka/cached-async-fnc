df0f82ccc7159bb9ec1ab7051079546ea1bfc257
Yannic <y.kalka@outlook.com>
2022-03-25 14:58:08 +0100

    Add test case

--- src/create-cached-async-fnc.test.ts
+++ src/create-cached-async-fnc.test.ts
@@ -2,6 +2,10 @@ import test from "ava";
 import { createCachedAsyncFnc } from "./create-cached-async-fnc";
 
 const resolveFunction = async (name: string, age: number) => {
+  if (age === 0) {
+    return undefined;
+  }
+
   return `${name} is ${age} years old`;
 };
 
@@ -16,4 +20,10 @@ test("New calls are added to the cache", async (t) => {
 test("Returns the data of the resolveFunction", async (t) => {
   const cached = await cachedFnc.get("Max", 23);
   t.is(cached, "Max is 23 years old");
+});
+
+test("Cache results for functions that return undefined", async (t) => {
+  t.false(cachedFnc.has("Max", 0));
+  await cachedFnc.get("Max", 0);
+  t.true(cachedFnc.has("Max", 0));
 });
